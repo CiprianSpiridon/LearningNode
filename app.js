24d3601b6ab27923fd3ecaf767acca7bd4445785
Ciprian Spiridon <contact@ciprianspiridon.co.uk>
2015-07-25 21:44:39 +0100

    First module

---
+++ app.js
@@ -0,0 +1,4 @@
+var circle = require('./modules/circle');
+
+var radius = 1;
+console.log("Area of the circle with radius of 1 is:", circle.area(radius));
---
+++ modules/circle.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ciprianspiridon on 25/07/15.
+ */
+module.exports = {
+    area: function (radius) {
+        return radius * radius * Math.PI;
+    },
+    circumference: function (radius) {
+        return 2 * radius * Math.PI;
+    }
+
+};
